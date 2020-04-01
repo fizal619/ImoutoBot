5c94c4e5d2ece4866336acfdae7944cc608cd854
Fizal Sarif <fizal.sarif@gmail.com>
2020-04-01 00:42:57 +0000

    savepoint 🤤

--- main.rb
+++ main.rb
@@ -4,28 +4,32 @@ require 'rufus-scheduler'
 
 require "./covid.rb"
 
+# SINATRA STUFF
 set :bind, "0.0.0.0"
 set :protection, :except => :frame_options
 
 get "/" do
   erb :index,  :locals => { host: request.host }
 end
+# END SINATRA STUFF
 
 @bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"]
 
-scheduler = Rufus::Scheduler.singleton
-scheduler.every "10s" do
-  # @bot.send_message("op_af", covid)
-end
+# scheduler = Rufus::Scheduler.singleton
+# scheduler.every "6h" do
+#   # @bot.send_message("op_af", covid)
+# end
 
 Thread.abort_on_exception = true
 Thread.new {
-  # DONT CHANGE ABOVE THIS LINE
+  # DONT CHANGE ABOVE THIS LINE ====================================================
   # MIND YOUR INDENTATION PLS TY
   # Ruby Examples: https://github.com/discordrb/discordrb/tree/master/examples
   # NOTE: VOICE DOESN"T WORK ON REPLIT SADLY
+  # Press start/restart to apply changes to imoutobot
 
   @bot.message(with_text: "imouto covid") do |event|
+    # this is a function from covid.rb
     event.respond covid
   end
 
@@ -41,7 +45,11 @@ Thread.new {
     event.respond "d-d-daisuki"
   end
 
-  @bot.run
+  @bot.message(with_text: "imouto levi") do |event|
+    event.respond "😏 \n
+    https://media.discordapp.net/attachments/128661000239972353/686412405823111183/ItachiLevi.gif"
+  end
 
-  # DONT CHANGE BELOW THIS LINE
+  # DONT CHANGE BELOW THIS LINE ==================================================
+  @bot.run
 }
